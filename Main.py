80e92f6c2b214660dd4868c73ce7de8c030ab1ce
Marco Pacchiotti <ICTS22-24.016@studenti.local>
2023-03-30 11:17:20 +0200

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+while True:
+    try:
+        a = input("inserire a:")
+        break
+    except:
+        print("errore")
+operatore = input("che operazione vuoi fare")
+match operatore:
+    case '+'
